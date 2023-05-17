ed18a13327044462e38399bf511cac773dd7d42b
sulkhan <sulkhangalangsakti@gmail.com>
2023-05-17 22:34:54 +0700

    feat : add font and basic navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,54 @@
-import { React, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-// import logo from "../logo.png";
 import "../index.css";
 
-let Navbar = () => {
-  let activeClassName =
-    "text-xl uppercase font-bold tracking-wide transition-colors duration-200 text-yellow-500  border-b-4 border-green-700 ";
-  let inactiveClassname =
-    "text-xl uppercase font-bold tracking-wide text-green-600 transition-colors duration-200 hover:text-yellow-500 hover:border-b-4 hover:border-green-700";
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const navbarColorClass = scrolled ? "bg-white " : "bg-transparent";
+  const navbarTextClass = scrolled ? "text-[#717171] hover:border-b-4 hover:border-red-700 " : "text-gray-100 hover:border-b-4 hover:border-gray-100 border-gray-100";
+  const ramenClass = scrolled
+    ? "text-red-500 text-2xl transition-all duration-300"
+    : "text-green-600 text-4xl transition-all duration-300";
+  const activeClassName =
+    "text-xl  font-bold tracking-wide transition-colors duration-200 text-yellow-500 border-b-4 border-red-700" + " " + navbarTextClass;
+  const inactiveClassname =
+    "text-xl  font-bold tracking-wide  transition-colors duration-200 0 hover:border-b-4 hover:border-green-700" + " " + navbarTextClass;
+
   return (
     <div>
-      <div className="border-b-4 fixed h-28 bg-yellow-50 right-0 left-0 z-40  py-6">
-        <div className="relative flex  w-full px-24 lg:flex-nowrap justify-around ">
+      <div
+        className={` fixed h-28 right-0 left-0 z-40 py-4 ${navbarColorClass} flex flex-col justify-between items-center`}
+        style={{
+          transition: "background-color 0.6s ease-out", // Add transition CSS property
+        }}
+      >
+        <NavLink
+           to="/"
+           className={({ isActive }) =>
+             isActive ? activeClassName : inactiveClassname
+           }
+         >
+           <h1 className={`font-bold ${ramenClass}`}>Ramen</h1>
+         </NavLink>
           <ul className="flex items-center mx-16 gap-16 space-x-10 lg:flex">
             <li>
               <NavLink
                 to="/reservation"
-                className={({ isActive }) =>
-                  isActive ? activeClassName : inactiveClassname
+                className={({ isActive}) =>
+                  isActive ? activeClassName : inactiveClassname 
                 }
               >
                 Reservation
@@ -33,19 +64,6 @@ let Navbar = () => {
                 Menu
               </NavLink>
             </li>
-          </ul>
-
-          {/* <img src={logo}></img> */}
-
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive ? activeClassName : inactiveClassname
-                }
-              >
-                <h1 className="text-4xl font-bold text-green-600">Ramen</h1>
-              </NavLink>
-          <ul className="mx-16 gap-16 flex items-center space-x-10 lg:flex">
             <li>
               <NavLink
                 to="/contact"
@@ -67,9 +85,11 @@ let Navbar = () => {
               </NavLink>
             </li>
           </ul>
+
+       
         </div>
-      </div>
     </div>
   );
 };
+
 export default Navbar;
