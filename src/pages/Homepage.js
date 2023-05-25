c98adf2567f70146d2c8e36ef0a5e984fe2cd821
sulkhan <sulkhangalangsakti@gmail.com>
2023-05-25 21:57:01 +0700

    feat : add section to homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,6 +4,7 @@ import "../index.css";
 import backgroundSectionTwo from "../assets/image/bg-section-2.jpg";
 import logoColor from "../assets/image/logo-color.png";
 import MoonLoader from "react-spinners/MoonLoader";
+import backgroundHomepage from "../assets/image/background-homepage.jpg";
 
 let Homepage = () => {
   let [loading, setLoading] = useState(true);
@@ -50,23 +51,50 @@ let Homepage = () => {
             border="0"
             className="mx-auto w-40 py-8"
           />
-          <p className="text-lg mb-4 text-center">
-            The concept of "Ramen Restaurant G" originated from a deep passion
+          <p className="text-lg mb-4 text-justify sm:text-center">
+          &nbsp; The concept of "Ramen Restaurant G" originated from a deep passion
             for exquisite cuisine and cultural experiences.
           </p>
-          <p className="text-lg mb-4 text-center">
-            Having dedicated years to meticulous planning and refinement, we are
+          <p className="text-lg mb-4 text-justify sm:text-center">
+          &nbsp; Having dedicated years to meticulous planning and refinement, we are
             thrilled to present you with an extraordinary dish: Yunnan's
             renowned "crossing-the-bridge" noodles.
           </p>
-          <p className="text-lg mb-4 text-center">
-            Embark on a culinary journey with us as we showcase the freshness of
+          <p className="text-lg mb-4 text-justify sm:text-center">
+          &nbsp; Embark on a culinary journey with us as we showcase the freshness of
             our thoughtfully sourced ingredients. Delight in the harmony of
             flavors as our signature base broth, painstakingly simmered using
             premium chicken bones for up to 10 hours, intermingles with the
             texture of our delectable rice noodles, prepared right at your
             table.
           </p>
+        </div>
+      </div>
+      <div
+        data-aos="fade-down"
+        data-aos-duration="3000"
+        className="  inset-0 flex flex-col z-10"
+      >
+        <div className="relative flex items-center justify-center ">
+          <div className="h-[80vh] absolute inset-0 bg-image-navbar opacity-40 -z-20"></div>
+          <div className=" absolute inset-0 bg-black opacity-70 -z-10"></div>
+          <img
+            src={backgroundHomepage}
+            className="absolute h-[80vh] w-full object-cover inset-0 -z-30"
+          />
+
+          <ul className="h-[80vh] flex flex-col items-center justify-center gap-8 sm:gap-20 w-4/5 sm:w-3/4">
+            <li className="text-3xl sm:text-5xl text-gray-50 font-semibold text-center">
+              The Enduring Delight of a Satisfied Soul
+            </li>
+            <li>
+              <button
+                type="submit"
+                className="text-white py-2 px-4 rounded-3xl border-2 border-white hover:bg-gray-50 hover:text-gray-700 transition duration-600"              >
+                &nbsp; &nbsp; About Us &nbsp; &nbsp;
+              </button>{" "}
+            </li>
+          </ul>
         </div>
       </div>
     </div>
