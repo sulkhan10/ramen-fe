c57bf9a29e4f213f443e13f88fc5c354efc03aa8
sulkhan <sulkhangalangsakti@gmail.com>
2023-05-29 10:22:16 +0700

    feat : add section to menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -11,8 +11,10 @@ import {
   IoArrowBackCircleOutline,
   IoRemove,
 } from "react-icons/io5";
-import { useMediaQuery } from 'react-responsive';
-
+import { useMediaQuery } from "react-responsive";
+import backgroundMenu from "../assets/image/background-menu-1.webp";
+import cardMenuOne from "../assets/image/card-menu-1.jpg";
+import cardMenuTwo from "../assets/image/card-menu-2.jpg";
 import MoonLoader from "react-spinners/MoonLoader";
 
 SwiperCore.use([Navigation]);
@@ -39,18 +41,18 @@ const Menu = () => {
   const [isNextHovered, setIsNextHovered] = useState(false);
   const [isPrevHovered, setIsPrevHovered] = useState(false);
 
-  const isSmallScreen = useMediaQuery({ query: '(max-width: 640px)' });
+  const isSmallScreen = useMediaQuery({ query: "(max-width: 640px)" });
 
   const handleClickNext = () => {
     goNext();
-  
+
     if (isSmallScreen) {
       handleMouseLeaveNext();
     }
   };
   const handleClickPrev = () => {
     goPrev();
-  
+
     if (isSmallScreen) {
       handleMouseLeavePrev();
     }
@@ -74,8 +76,41 @@ const Menu = () => {
   const iconSizePrev = isPrevHovered ? "scale-125" : "scale-100";
 
   return (
-    <div className="min-h-screen bg-red-400 flex justify-center items-center text-4xl text-white ">
-      <div className="w-full bg-sky-600 relative">
+    <div className="min-h-screen bg-[#f1eee9] flex flex-col  justify-center items-center text-md text-[#717171] ">
+      <div className="h-screen ">
+        <img src={backgroundMenu} className="h-full object-cover" />
+      </div>
+      <div className="flex">
+        <div className="h-auto w-1/4 overflow-hidden">
+          <img
+            src={cardMenuOne}
+            alt="card menu"
+            className="h-full w-full object-cover transform transition-transform duration-700 hover:scale-125	"
+          />
+        </div>
+        <div className="h-auto w-1/4 overflow-hidden">
+          <img
+            src={cardMenuOne}
+            alt="card menu"
+            className="h-full w-full object-cover transform transition-transform duration-700 hover:scale-125	"
+          />
+        </div>
+        <div className="h-auto w-1/4 overflow-hidden">
+          <img
+            src={cardMenuOne}
+            alt="card menu"
+            className="h-full w-full object-cover transform transition-transform duration-700 hover:scale-125	"
+          />
+        </div>
+        <div className="h-auto w-1/4 overflow-hidden">
+          <img
+            src={cardMenuOne}
+            alt="card menu"
+            className="h-full w-full object-cover transform transition-transform duration-700 hover:scale-125	"
+          />
+        </div>
+      </div>
+      <div className="w-full sm:h-[70vh] relative p-12 mb-20 ">
         <Swiper
           ref={swiperRef}
           slidesPerView="auto"
@@ -85,72 +120,242 @@ const Menu = () => {
           onSwiper={(swiper) => console.log(swiper)}
         >
           <SwiperSlide>
-            <div className="bg-sky-100">
-              Slide 1
-              <div className="flex gap-8 bg-gray-100">
+            <div className="flex gap-8 bg-[#f5f0ec] flex-col sm:flex-row  ">
+              <div className="w-full sm:w-1/3 flex flex-col gap-6">
+                <p className="cursor-default text-xl font-semibold tracking-wider	">
+                  SPECIAL TASTE
+                </p>
+                <p className="cursor-default text-5xl font-semobild text-[#cb6037] py-2 tracking-wider">
+                  PIZZA
+                </p>
+                <p className="cursor-default text-lg 	">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+              <div className="w-full sm:w-1/3 flex flex-col gap-6">
                 <div>
-                  SPECIAL TASTE PIZZA Lorem ipsum dolor sit amet, consectetur
-                  adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua.
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      MARGHERITA
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p className="cursor-default">Vegeterian</p>
+                </div>
+
+                <div>
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      CALZONE
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p>Meat</p>
                 </div>
                 <div>
-                  MARGHERITA $ 11.99 USD Vegeterian СALZONE $ 13.99 USD Meat
-                  PEPPERONI $ 12.99 USD Meat HAWAIIAN $ 12.99 USD Meat
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      PEPPERONI
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p>Meat</p>
                 </div>
                 <div>
-                  TRIPLE MUSHROOM $ 12.99 USD Vegeterian MEAT FEAST $ 12.99 USD
-                  Meat QUATTRO FORMAGGI $ 12.99 USD Vegeterian TONNO $ 12.99 USD
-                  Fish
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      HAWAIIAN
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p>Meat</p>
+                </div>
+              </div>
+              <div className="w-full sm:w-1/3 flex flex-col gap-6">
+                <div>
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      TRIPLE MUSHROOM
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p className="cursor-default">Vegeterian</p>
+                </div>
+                <div>
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      MEAT FEAST
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p>Meat</p>
+                </div>
+                <div>
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      QUATTRO FORMAGGI
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p className="cursor-default">Vegeterian</p>
+                </div>
+                <div>
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      TONNO
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p>Fish</p>
                 </div>
               </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className="bg-sky-100">
-              Slide 2
-              <div className="flex gap-8 bg-gray-100">
+            <div className="flex gap-8 bg-[#f5f0ec] flex-col sm:flex-row  ">
+              <div className="w-full sm:w-1/3 flex flex-col gap-6">
+                <p className="cursor-default text-xl font-semibold tracking-wider	">
+                  SPECIAL TASTE
+                </p>
+                <p className="cursor-default text-5xl font-semobild text-[#cb6037] py-2 tracking-wider">
+                  PIZZA
+                </p>
+                <p className="cursor-default text-lg	">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+              <div className="w-full sm:w-1/3 flex flex-col gap-6">
                 <div>
-                  SPECIAL TASTE PIZZA Lorem ipsum dolor sit amet, consectetur
-                  adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua.
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      MARGHERITA
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p className="cursor-default">Vegeterian</p>
+                </div>
+
+                <div>
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      CALZONE
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p>Meat</p>
                 </div>
                 <div>
-                  MARGHERITA $ 11.99 USD Vegeterian СALZONE $ 13.99 USD Meat
-                  PEPPERONI $ 12.99 USD Meat HAWAIIAN $ 12.99 USD Meat
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      PEPPERONI
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p>Meat</p>
                 </div>
                 <div>
-                  TRIPLE MUSHROOM $ 12.99 USD Vegeterian MEAT FEAST $ 12.99 USD
-                  Meat QUATTRO FORMAGGI $ 12.99 USD Vegeterian TONNO $ 12.99 USD
-                  Fish
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      HAWAIIAN
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p>Meat</p>
                 </div>
               </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="bg-sky-100">
-              Slide 3
-              <div className="flex gap-8 bg-gray-100">
+              <div className="w-full sm:w-1/3 flex flex-col gap-6">
                 <div>
-                  SPECIAL TASTE PIZZA Lorem ipsum dolor sit amet, consectetur
-                  adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua.
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      TRIPLE MUSHROOM
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p className="cursor-default">Vegeterian</p>
                 </div>
                 <div>
-                  MARGHERITA $ 11.99 USD Vegeterian СALZONE $ 13.99 USD Meat
-                  PEPPERONI $ 12.99 USD Meat HAWAIIAN $ 12.99 USD Meat
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      MEAT FEAST
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p>Meat</p>
                 </div>
                 <div>
-                  TRIPLE MUSHROOM $ 12.99 USD Vegeterian MEAT FEAST $ 12.99 USD
-                  Meat QUATTRO FORMAGGI $ 12.99 USD Vegeterian TONNO $ 12.99 USD
-                  Fish
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      QUATTRO FORMAGGI
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p className="cursor-default">Vegeterian</p>
+                </div>
+                <div>
+                  <div className="flex gap-8 text-xl pb-2">
+                    <p className="cursor-default hover:text-[#cb6037] transition-colors duration-400 hover:cursor-pointer">
+                      TONNO
+                    </p>
+                    <p className="cursor-default text-[#cb6037]">
+                      {" "}
+                      $ 11.99 USD
+                    </p>
+                  </div>
+                  <p>Fish</p>
                 </div>
               </div>
             </div>
           </SwiperSlide>
         </Swiper>
-        <div className="absolute sm:left-0 bottom-4 w-1/3  left-1/3 sm:bottom-1/8 transform -translate-y-1/2 bg-red-100 z-10">
+        <div className="absolute sm:left-0 -bottom-16 w-1/3  left-1/3 sm:bottom-[20%] transform -translate-y-1/2 z-10 ">
           <div className="flex items-center justify-center">
-           
             <button
               className={`focus:outline-none transform transition-transform duration-200 ${iconSizePrev}`}
               onMouseEnter={handleMouseEnterPrev}
@@ -164,13 +369,15 @@ const Menu = () => {
               className={`focus:outline-none transform transition-transform duration-200 ${iconSizeNext}`}
               onMouseEnter={handleMouseEnterNext}
               onMouseLeave={handleMouseLeaveNext}
-              onClick={
-                handleClickNext
-              }            >
+              onClick={handleClickNext}
+            >
               <IoArrowForwardCircleOutline size={32} color="#cf6e49" />
             </button>
           </div>
         </div>
+      </div>
+      <div className="h-screen ">
+        <img src={backgroundMenu} className="h-full object-cover" />
       </div>
     </div>
   );
